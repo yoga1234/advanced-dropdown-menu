563ba5dcd60ed3071ecd626be593f279640af5a3
Septiana Yoga <septianayoga30@gmail.com>
2021-09-10 18:34:50 +0700

    adding icons to the ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+import { ReactComponent as CaretIcon } from "./icons/caret.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as CogIcon } from "./icons/cog.svg";
+import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
+import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
+import { ReactComponent as BoltIcon } from "./icons/bolt.svg";
 
 function App() {
   return (
     <Navbar>
-      <NavItem icon="O" />
-      <NavItem icon="O" />
-      <NavItem icon="O" />
+      <NavItem icon={<PlusIcon />}/>
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
     </Navbar>
-  )
+  );
 }
 
 function Navbar(props) {
   return (
     <nav className="navbar">
-      <ul className="navbar-nav">{ props.children}</ul>
+      <ul className="navbar-nav">{props.children}</ul>
     </nav>
-  )
+  );
 }
 
 function NavItem(props) {
@@ -25,7 +34,7 @@ function NavItem(props) {
         {props.icon}
       </a>
     </li>
-  )
+  );
 }
 
-export default App
+export default App;
