b37690c66d985dbc3219bb0e484522b33ab06abd
Septiana Yoga <septianayoga30@gmail.com>
2021-09-10 18:28:59 +0700

    adding icons and button styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Navbar>
+      <NavItem icon="O" />
+      <NavItem icon="O" />
+      <NavItem icon="O" />
+    </Navbar>
+  )
+}
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{ props.children}</ul>
+    </nav>
+  )
+}
+
+function NavItem(props) {
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
   )
 }
 
